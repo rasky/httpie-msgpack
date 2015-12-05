e1545e16c378c0fea4efb001b30fc9e65aad1bbe
Vitaly Babiy <vbabiy86@gmail.com>
2015-12-05 10:16:45 -0500

    Support any msgpack mimetype

--- httpie_msgpack.py
+++ httpie_msgpack.py
@@ -1,15 +1,16 @@
-from httpie.plugins import ConverterPlugin
 import json
+
 import msgpack
+from httpie.plugins import ConverterPlugin
 
 DEFAULT_INDENT = 4
 
-class MsgpackPlugin(ConverterPlugin):
 
+class MsgpackPlugin(ConverterPlugin):
     @classmethod
     def supports(cls, mime):
-      return 'x-msgpack' in mime
+        return 'msgpack' in mime
 
     def convert(self, body):
         return "application/json", \
-            json.dumps(msgpack.loads(body), encoding="unicode-escape", sort_keys=True)
+               json.dumps(msgpack.loads(body), encoding="unicode-escape", sort_keys=True)
